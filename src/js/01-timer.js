1d068cac9b2aff4ab0e46c55380d526056c5fa06
MaciejBratko <maciej.bratko@outlook.com>
2024-07-08 00:26:12 +0200

    disable the date picker after pressing start

--- src/js/01-timer.js
+++ src/js/01-timer.js
@@ -2,12 +2,10 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 let userSelectedDate;
-
 const startButton = document.querySelector('button[data-start]');
+const datePickerInput = document.querySelector('#datetime-picker');
 startButton.disabled = true;
-
 const validateDate = selectedDate => {
   const now = Date.now();
   if (selectedDate <= now) {
@@ -15,7 +13,6 @@ const validateDate = selectedDate => {
   }
   return Promise.resolve(selectedDate);
 };
-
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -24,7 +21,6 @@ const options = {
   onClose(selectedDates) {
     const selectedDate = selectedDates[0];
     console.log(selectedDate);
-
     validateDate(selectedDate)
       .then(validDate => {
         userSelectedDate = validDate;
@@ -46,29 +42,22 @@ const options = {
       });
   },
 };
-
 flatpickr('#datetime-picker', options);
-
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   const days = Math.floor(ms / day);
   const hours = Math.floor((ms % day) / hour);
   const minutes = Math.floor((ms % hour) / minute);
   const seconds = Math.floor((ms % minute) / second);
-
   return { days, hours, minutes, seconds };
 }
-
 let countdown;
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
+function disableDatePicker() {
+  datePickerInput.disabled = true;
 }
-
 function updateTimerInterface({ days, hours, minutes, seconds }) {
   document.querySelector('span[data-days]').textContent = addLeadingZero(days);
   document.querySelector('span[data-hours]').textContent =
@@ -78,27 +67,24 @@ function updateTimerInterface({ days, hours, minutes, seconds }) {
   document.querySelector('span[data-seconds]').textContent =
     addLeadingZero(seconds);
 }
-
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
 function startTimer() {
   startButton.disabled = true;
-
+  disableDatePicker();
   const updateTimer = () => {
     const now = Date.now();
     const timeDifference = userSelectedDate - now;
-
     if (timeDifference <= 0) {
       clearInterval(countdown);
       updateTimerInterface(convertMs(0));
       return;
     }
-
     const timeLeft = convertMs(timeDifference);
     updateTimerInterface(timeLeft);
   };
-
   updateTimer();
-
   countdown = setInterval(updateTimer, 1000);
 }
-
 startButton.addEventListener('click', startTimer);
